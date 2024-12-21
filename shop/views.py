853f8655c06511c62907ec1ac8b18c6108436579
dheeraj <dheerajchauhanjodi@gmail.com>
2024-12-21 12:22:55 +0530

    remove some unnecessary

--- shop/views.py
+++ shop/views.py
@@ -1,23 +1,10 @@
-from django.shortcuts import render, redirect,get_object_or_404
-from .models import Product
-from django.http import JsonResponse
+from django.shortcuts import render, redirect
 import stripe
 stripe.api_key = "sk_test_51QWwwTLgKEZ8FUNSILqkqMmHvtD6VaxD09dWpObjSlSBlXplUjOf03sCaWcPm65Wc0nbMV75UdKM0UDoL9KP9yrm006zFPtWio"
 
 
 def product_list(request):
     products = stripe.Product.list()
-
-    product_data = []
-    for product in products['data']:
-        price = stripe.Price.list(product=product['id'])
-        product_data.append({
-            'name': product['name'],
-            'description': product['description'],
-            'price': price['data'][0]['unit_amount'] / 100,
-            'stripe_product_id': product['id'],
-            'stripe_price_id': price['data'][0]['id'],
-        })
     return render(request, 'shop/product_list.html', {'products': products})
 
 def checkout(request,stripe_product_id):
